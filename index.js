01fd993d3a109d98e5f4da05e934c541b31d19a3
suqing-su <1814726029@qq.com>
2026-04-12 18:58:13 +0800

    add chat api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const https = require('https');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
 res.header('Access-Control-Allow-Origin', '*');
 res.header('Access-Control-Allow-Headers', 'Content-Type');
